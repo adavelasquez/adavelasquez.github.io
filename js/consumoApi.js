ee3a828206aba7c9b0c45c45cc4dd58d9a336cbf
adavelasquez <belen.velasquez96@gmail.com>
2022-10-27 22:37:55 -0300

    agregado de nueva API

--- js/consumoApi.js
+++ js/consumoApi.js
@@ -1,16 +1,21 @@
-const direccion = "https://api.unidadeditorial.es/sports/v1/classifications/current/?site=2&type=10&tournament=0152"; // no deja acceder por error de cors, se copio la informacion en data.json 
+const API_KEY = "e1dd3c21162e5dceeac6783c306e1352cdf7a0bb88fc6ba64605e3805172b78f";
+const direccion = `https://apiv2.allsportsapi.com/football/?met=Countries&APIkey=${API_KEY}`; // no deja acceder por error de Cors, se copio la informacion en data.json 
 
 // crear async
 async function obtenerDatos(){
-    const response = await fetch("./js/data.json")
+    const response = await fetch(direccion);
     const data = await response.json();
-    const equipos = data.data[0].rank;
-
+    const equipos = data.result;
     for(let i = 0 ; i < equipos.length; i++){
         let imgEquipo = document.createElement("img");
-        let imageUrl = equipos[i].imageUrlSizes.L;
+        br = document.createElement("span");
+        br.innerHTML = "<br/>";
+        let imageUrl = equipos[i].country_logo;
         imgEquipo.setAttribute("src", imageUrl);
         document.getElementById("equipos").appendChild(imgEquipo);
+        document.getElementById("equipos").appendChild(br);
+        
+
     }
 
 }
